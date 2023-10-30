83fa86e5432616b4f0128fbf4cf360c5fc4c53b2
hs_junxiang <jimmy871117@gmail.com>
2023-10-30 11:34:48 +0800

    feat: use @chat to chat with bot in group or room (#1)

--- chatgpt_linebot/memory.py
+++ chatgpt_linebot/memory.py
@@ -14,39 +14,44 @@ class MemoryInterface:
 
 
 class Memory(MemoryInterface):
-    """
-    storage = [
-        {'role': 'system', 'content': 'You are a helpful assistant.'},
-        {'role': 'user', content': 'Hi'},
-        {'role': 'system', 'content': 'Hi. How can i assist u?'},
-        ...
-    ]
+    """Chat Memory
+    
+    Args:
+        storage (List[Dict[str, str]]): Chat history, ex: 
+        [
+            {'role': 'system', 'content': 'You are a helpful assistant.'},
+            {'role': 'user', content': 'Hi'},
+            {'role': 'system', 'content': 'Hi. How can i assist u?'},
+            ...
+        ]
+
+        id (int): user_id, grouop_id, room_id
     """
     def __init__(self, memory_message_count: int) -> None:
         self.storage = defaultdict(list)
         self.memory_message_count = memory_message_count
 
-    def _initialize(self, user_id: str) -> None:
-        self.storage[user_id] = [{
+    def _initialize(self, id: str) -> None:
+        self.storage[id] = [{
             'role': 'system', 'content': 'You are a helpful assistant.'
         }]
 
-    def _drop_message(self, user_id: str) -> str:
-        if len(self.storage.get(user_id)) >= (self.memory_message_count + 1) * 2 + 1:
-            return [self.storage[user_id][0]] + self.storage[user_id][-(self.memory_message_count * 2):]
-        return self.storage.get(user_id)
+    def _drop_message(self, id: str) -> str:
+        if len(self.storage.get(id)) >= (self.memory_message_count + 1) * 2 + 1:
+            return [self.storage[id][0]] + self.storage[id][-(self.memory_message_count * 2):]
+        return self.storage.get(id)
 
-    def append(self, user_id: str, role: str, content: str) -> None:
-        if self.storage[user_id] == []:
-            self._initialize(user_id)
-        self.storage[user_id].append({
+    def append(self, id: str, role: str, content: str) -> None:
+        if self.storage[id] == []:
+            self._initialize(id)
+        self.storage[id].append({
             'role': role,
             'content': content
         })
-        self._drop_message(user_id)
+        self._drop_message(id)
 
-    def get(self, user_id: str) -> str:
-        return self.storage[user_id]
+    def get(self, id: str) -> str:
+        return self.storage[id]
 
-    def remove(self, user_id: str) -> None:
-        self.storage[user_id] = []
+    def remove(self, id: str) -> None:
+        self.storage[id] = []
--- chatgpt_linebot/urls.py
+++ chatgpt_linebot/urls.py
@@ -44,17 +44,18 @@ async def callback(request: Request) -> str:
     return "OK"
 
 
-def chat_completion(user_id: int) -> str:
+def chat_completion(id: int) -> str:
+    """Use OpenAI API via gpt4free providers"""
     try:
         response = g4f.ChatCompletion.create(
             model=g4f.models.default,
-            messages=memory.get(user_id),
+            messages=memory.get(id),
         )
-        memory.append(user_id, 'system', response)
+        memory.append(id, 'system', response)
 
     except Exception as e:
         response = (
-        f"There're something wrong, please try again.\n"
+        "There're something wrong in openai api, please try again.\n"
         "Or connect to developer: https://github.com/Lin-jun-xiang/chatgpt-line-bot/issues"
         )
         print(e)
@@ -67,8 +68,10 @@ def handle_message(event) -> None:
     """Event - User sent message
 
     Args:
-        event (LINE Event Object): Refer to https://developers.line.biz/en/reference/messaging-api/#message-event
+        event (LINE Event Object)
+
     Refs:
+        https://developers.line.biz/en/reference/messaging-api/#message-event
         https://www.21cs.tw/Nurse/showLiangArticle.xhtml?liangArticleId=503
     """
     if not isinstance(event.message, TextMessage):
@@ -76,24 +79,28 @@ def handle_message(event) -> None:
 
     reply_token = event.reply_token
     user_id = event.source.user_id
+    display_name = line_bot_api.get_profile(user_id).display_name # user name
+    response = None
+
+    # Get user sent message
+    pre_prompt = girlfriend
+    user_message = f"{pre_prompt}:\n{event.message.text}"
 
     if event.source.type == 'user':
-
-        # Get user sent message
-        pre_prompt = girlfriend
-        user_message = f"{pre_prompt}:\n{event.message.text}"
         memory.append(user_id, 'user', user_message)
         response = chat_completion(user_id)
 
-    elif event.source.type == 'group':
+    elif event.source.type == 'group' and event.message.text.startswith('@chat'):
         group_id = event.source.group_id
-
-        # Get user sent message
-        pre_prompt = girlfriend
-        user_message = f"{pre_prompt}:\n{event.message.text}"
-        memory.append(group_id, str(user_id), user_message)
+        memory.append(group_id, 'user', user_message.replace('@chat', ''))
         response = chat_completion(group_id)
 
+    elif event.source.type == 'room' and event.message.text.startswith('@chat'):
+        room_id = event.source.room_id
+        memory.append(room_id, 'user', user_message.replace('@chat', ''))
+        response = chat_completion(room_id)
+
     # Reply with same message
-    messages = TextSendMessage(text=response)
-    line_bot_api.reply_message(reply_token=reply_token, messages=messages)
+    if response:
+        messages = TextSendMessage(text=response)
+        line_bot_api.reply_message(reply_token=reply_token, messages=messages)
